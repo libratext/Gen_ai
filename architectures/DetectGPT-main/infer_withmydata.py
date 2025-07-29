b71cff59e78ace82db34458103779c09df5c9116
Alice Breton <alice.breton.lip6@gmail.com>
2025-07-29 13:40:26 +0200

    Fix none prediction and output file name changing with config file

--- architectures/DetectGPT-main/infer_withmydata.py
+++ architectures/DetectGPT-main/infer_withmydata.py
@@ -1,11 +1,11 @@
 import json
-import time
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 from model import GPT2PPLV2 as GPT2PPL
 
 import time
 import yaml
+import os
 
 start_time = time.time()
 
@@ -30,7 +30,7 @@ def map_prediction_to_label(prediction_text):
         return 1
     else:
         print(f"Unexpected prediction text: {prediction_text}")
-        return -1
+        return None
 
 def evaluate_model(human_file_path, generated_file_path, output_predictions_path, output_metrics_path):
     human_data = load_json_file(human_file_path)
@@ -74,9 +74,13 @@ def evaluate_model(human_file_path, generated_file_path, output_predictions_path
         }
         append_to_json_file(prediction_data, output_predictions_path)
 
-    precision = precision_score(y_true, y_pred)
-    recall = recall_score(y_true, y_pred)
-    f1 = f1_score(y_true, y_pred)
+    valid_indices = [i for i, label in enumerate(y_pred) if label is not None]
+    y_true_filtered = [y_true[i] for i in valid_indices]
+    y_pred_filtered = [y_pred[i] for i in valid_indices]
+
+    precision = precision_score(y_true_filtered, y_pred_filtered)
+    recall = recall_score(y_true_filtered, y_pred_filtered)
+    f1 = f1_score(y_true_filtered, y_pred_filtered)
 
     metrics_data = {
         "precision": precision,
@@ -108,8 +112,8 @@ generated_file_path = (
         else config['datasets']['default_gen']
     )
 
-output_predictions_path = './results/DetectGPT/kaggle_predictions_output.json'
-output_metrics_path = './results/DetectGPT/kaggle_evaluation_metrics.json'
+output_predictions_path = './results/DetectGPT/'+os.path.basename(generated_file_path)+'_predictions_output.json'
+output_metrics_path = './results/DetectGPT/'+os.path.basename(generated_file_path)+'_evaluation_metrics.json'
 
 precision, recall, f1 = evaluate_model(human_file_path, generated_file_path, output_predictions_path, output_metrics_path)
 
